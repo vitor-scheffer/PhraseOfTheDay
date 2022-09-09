38953f9272a2727f28f2e2a33e7312cf89472627
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-09-09 18:11:09 -0300

    Initial Commit

---
+++ FraseDoDia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FraseDoDia
+//
+//  Created by José Vitor Scheffer Boff dos Santos on 09/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
