32be669bab82966063681d3f80229eed168d19ee
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-09-12 13:05:50 -0300

    Projeto frase do dia.

--- FraseDoDia/ViewController.swift
+++ FraseDoDia/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var phraseLabel: UILabel!
+    @IBAction func generatePhrase(_ sender: Any) {
+        let randomNumber = Int.random(in: 0..<3)
+        var phrasesArr: [String] = []
+        phrasesArr.append("Seja muito bem vindo ao nosso App! Sempre será um prazer atendê-lo.")
+        phrasesArr.append("É um prazer muito grande ter você aqui conosco.")
+        phrasesArr.append("Que sua experiência seja extraordinária aqui!!")
+        phraseLabel.text = phrasesArr[randomNumber]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
